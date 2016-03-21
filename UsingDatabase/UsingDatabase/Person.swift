de6a6b2d8ec5c0f232fa2311d8c097921f37be23
morshed <t.morshed1370@gmail.com>
2016-03-21 13:53:17 +0600

    database integration complete on primary level

---
+++ UsingDatabase/UsingDatabase/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  UsingDatabase
+//
+//  Created by Sekai Lab BD on 3/21/16.
+//  Copyright © 2016 Sekai Lab BD. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class Person: Object {
+    dynamic var name = ""
+    dynamic var age = ""
+    dynamic var des = ""
+}
--- UsingDatabase/UsingDatabase/ViewController.swift
+++ UsingDatabase/UsingDatabase/ViewController.swift
@@ -7,17 +7,41 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var ageText: UITextField!
+    @IBOutlet weak var desText: UITextField!
+    
+    @IBOutlet weak var warningLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        warningLabel.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func addToRealmDB(sender: AnyObject) {
+//        let person = Person()
+//        person.name = nameText.text!
+//        person.age = ageText.text!
+//        person.des = desText.text!
+//        
+//        let realm = try! Realm()
+//        //let persons = realm.objects(Person)
+//        try! realm.write({
+//            realm.add(person)
+//        })
+        
+        let realm = try! Realm()
+        let persons = realm.objects(Person)
+        print(persons)
+
     }
 
 
